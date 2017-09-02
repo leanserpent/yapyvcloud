91f11b05e298226c0079e7ff926733e62ecec81c
lean.serpent <lean.serpent@gmail.com>
2017-09-02 13:18:59 +1000

    update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ try:
 except ImportError:
     from distutils.core import setup
 config = {
-    'name': 'yapyvcloud'
-    'packages': ['yapyvcloud'],
-    'version': '0.1',
-    'description': 'yet another pyvcloud',
-    'author': 'lean.serpent',
-    'author_email': 'lean.serpent@gmail.com',
-    'url': 'https://github.com/leanserpent/yapyvcloud',
-    'download_url': 'https://github.com/leanserpent/yapyvcloud/archive/v0.1.tar.gz',
-    'keywords': ['python', 'vcloud']
-    'install_requires': ['nose'],
-    'scripts': [],
+    name = 'yapyvcloud'
+    packages = ['yapyvcloud'],
+    version = '0.1',
+    description = 'yet another pyvcloud',
+    author = 'lean.serpent',
+    author_email = 'lean.serpent@gmail.com',
+    url = 'https://github.com/leanserpent/yapyvcloud',
+    download_url = 'https://github.com/leanserpent/yapyvcloud/archive/v0.1.tar.gz',
+    keywords = ['python', 'vcloud']
+    install_requires = ['nose'],
+    scripts = [],
 }
 
 setup(**config)
