891b2bd61acff5f5e71ae0505a6c1181773ad77f
lean.serpent <lean.serpent@gmail.com>
2017-09-02 14:04:21 +1000

    update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ except ImportError:
 setup(
     name = 'yapyvcloud',
     packages = ['yapyvcloud'],
-    version = '0.1',
+    version = '0.2',
     description = 'yet another pyvcloud',
     author = 'lean.serpent',
     author_email = 'lean.serpent@gmail.com',
     url = 'https://github.com/leanserpent/yapyvcloud',
-    download_url = 'https://github.com/leanserpent/yapyvcloud/archive/v0.1.tar.gz',
+    download_url = 'https://github.com/leanserpent/yapyvcloud/archive/v0.2.tar.gz',
     keywords = ['python', 'vcloud'],
     install_requires = ['datetime','random','bs4','lxml'],
     scripts = []
