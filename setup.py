1635fd03cd5faed35d182287daecd343f953aad3
lean.serpent <lean.serpent@gmail.com>
2017-09-02 14:30:07 +1000

    update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ except ImportError:
 setup(
     name = 'yapyvcloud',
     packages = ['yapyvcloud'],
-    version = '0.3',
+    version = '0.4',
     description = 'yet another pyvcloud',
     author = 'lean.serpent',
     author_email = 'lean.serpent@gmail.com',
     url = 'https://github.com/leanserpent/yapyvcloud',
-    download_url = 'https://github.com/leanserpent/yapyvcloud/archive/v0.3.tar.gz',
+    download_url = 'https://github.com/leanserpent/yapyvcloud/archive/v0.4.tar.gz',
     keywords = ['python', 'vcloud'],
-    install_requires = ['bs4','lxml'],
+    install_requires = ['beautifulsoup4','lxml'],
     scripts = []
 )
 
